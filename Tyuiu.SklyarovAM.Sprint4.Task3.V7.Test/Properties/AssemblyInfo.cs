477d4b295b55550c799c901078100d20a074d746
Антон Скляров <sklarovanton833@gmail.com>
2026-06-10 13:22:42 +0500

    Task3

---
+++ Tyuiu.SklyarovAM.Sprint4.Task3.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SklyarovAM.Sprint4.Task3.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SklyarovAM.Sprint4.Task3.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4f1b229-4546-4f8a-905e-d5cad2220e43")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SklyarovAM.Sprint4.Task3.V7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SklyarovAM.Sprint4.Task3.V7.Lib;
+
+namespace Tyuiu.SklyarovAM.Sprint4.Task3.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] matrix = {
+            { 9, 6, 9, 3, 7 },
+            { 3, 3, 3, 8, 2 },
+            { 2, 1, 3, 5, 2 },
+            { 6, 2, 3, 2, 5 },
+            { 4, 5, 6, 9, 5 }
+            };
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(matrix));
+            Console.ReadKey();
+        }
+    }
+}
